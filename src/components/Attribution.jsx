d1087358a8903b9e92e8bce450aec965a6ae5c0b
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2024-05-30 13:15:51 -0400

    fix footer contrast

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,8 +1,8 @@
 export default function FrontendMentorAttributionFooter({name = '', socialMediaLink = "#"}) {
   return (
-    <footer className="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href={socialMediaLink}>{name}</a>.
+    <footer className="attribution" style={{color: "var(--primary-text)"}}>
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" style={{color: "var(--primary-accent)"}}>Frontend Mentor</a>. 
+      Coded by <a href={socialMediaLink}  style={{color: "var(--primary-accent)"}}>{name}</a>.
     </footer>
   )
 }
